92c81fc7c6675a0275874dee8e547194419d6c42
so6er <mrkvktrvch@gmail.com>
2021-10-27 19:06:25 +0300

    finished all checks

--- src/test/java/gmail/mrkvktrvch/TestingPracticeForm.java
+++ src/test/java/gmail/mrkvktrvch/TestingPracticeForm.java
@@ -1,5 +1,6 @@
 package gmail.mrkvktrvch;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.commands.UploadFile;
 import com.codeborne.selenide.selector.ByText;
@@ -9,6 +10,7 @@ import org.openqa.selenium.By;
 
 import java.io.File;
 
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -25,7 +27,7 @@ public class TestingPracticeForm {
     $("#firstName").setValue("Mark");
     $("#lastName").setValue("Kasimov");
     $("#userEmail").setValue("mrkvktrvch@gmail.com");
-    $$(".custom-radio").first().click();
+    $("[for=gender-radio-1]").click();
     $("#userNumber").setValue("1234567890");
     $("#dateOfBirthInput").click();
     $(".react-datepicker__month-select").selectOptionByValue("4");
@@ -37,16 +39,19 @@ public class TestingPracticeForm {
     $("#currentAddress").setValue("Saint-Petersburg");
     $("#react-select-3-input").setValue("ncr").pressEnter();
     $("#react-select-4-input").setValue("delhi").pressEnter();
-
-
-
-
-
-
-
-
-
+    $(".btn.btn-primary").pressEnter();
+    $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+    $$(".table-responsive tr").shouldHave(CollectionCondition.texts
+                ("Label Values",
+                "Student Name Mark Kasimov",
+                "Student Email mrkvktrvch@gmail.com",
+                "Gender Male",
+                "Mobile 1234567890",
+                "Date of Birth 16 May,1993",
+                "Subjects Arts",
+                "Hobbies Reading",
+                "Picture",
+                "Address Saint-Petersburg",
+                "State and City NCR Delhi"));
     }
-
-
 }
