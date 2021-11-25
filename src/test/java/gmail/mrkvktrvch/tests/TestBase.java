c8c104c5a45f71c2c90239dfd32dc63d89e36a56
so6er <mrkvktrvch@gmail.com>
2021-11-25 20:29:54 +0300

    added lambda steps

--- src/test/java/gmail/mrkvktrvch/tests/TestBase.java
+++ src/test/java/gmail/mrkvktrvch/tests/TestBase.java
@@ -1,7 +1,9 @@
 package gmail.mrkvktrvch.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import gmail.mrkvktrvch.pages.RegistrationPage;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -11,7 +13,8 @@ public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
 
     @BeforeAll
-    static void beforeAll() {
+    static void setup() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
         open("https://demoqa.com/automation-practice-form");
     }
