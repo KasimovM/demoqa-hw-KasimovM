1bd3b60e852e363c53cb2d357d113385ee1b8f10
so6er <mrkvktrvch@gmail.com>
2021-11-25 21:17:57 +0300

    removed lambda steps

--- src/test/java/gmail/mrkvktrvch/tests/TestingPracticeForm.java
+++ src/test/java/gmail/mrkvktrvch/tests/TestingPracticeForm.java
@@ -9,41 +9,30 @@ public class TestingPracticeForm extends TestData {
     @Test
     void registrationTest() {
         open("https://demoqa.com/automation-practice-form");
-
-        step("Fill students registration form", () -> {
-            step("Fill common data", () -> {
-                registrationPage
-                        .setFirstName(firstName)
-                        .setLastName(lastName)
-                        .setEmail(email)
-                        .chooseGender()
-                        .setNumber(phoneNumber);
-            });
-            step("Set date", () -> {
-                registrationPage.calendarComponent.setDateOfBirth("4", "1993", "16");
-            });
-            step("Set subjects, hobbies, address, state, city and download picture", () -> {
-                registrationPage
-                        .setSubjects("Arts")
-                        .clickHobbiesCheckBox()
-                        .downloadPicture()
-                        .setAddress(address)
-                        .setState("ncr")
-                        .setCity("delhi")
-                        .pressSubmitButton();
-            });
-            step("Verify successful form submit", () -> {
-                registrationPage.checkResultTable("Student Name", firstName + " " + lastName)
-                        .checkResultTable("Student Email", email)
-                        .checkResultTable("Gender", "Male")
-                        .checkResultTable("Mobile", phoneNumber)
-                        .checkResultTable("Date of Birth", "16 May,1993")
-                        .checkResultTable("Subjects", "Arts")
-                        .checkResultTable("Hobbies", "Reading")
-                        .checkResultTable("Picture", "structure.png")
-                        .checkResultTable("Address", address)
-                        .checkResultTable("State and City", "NCR Delhi");
-            });
-        });
+        registrationPage
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setEmail(email)
+                .chooseGender()
+                .setNumber(phoneNumber);
+        registrationPage.calendarComponent.setDateOfBirth("4", "1993", "16");
+        registrationPage
+                .setSubjects("Arts")
+                .clickHobbiesCheckBox()
+                .downloadPicture()
+                .setAddress(address)
+                .setState("ncr")
+                .setCity("delhi")
+                .pressSubmitButton();
+        registrationPage.checkResultTable("Student Name", firstName + " " + lastName)
+                .checkResultTable("Student Email", email)
+                .checkResultTable("Gender", "Male")
+                .checkResultTable("Mobile", phoneNumber)
+                .checkResultTable("Date of Birth", "16 May,1993")
+                .checkResultTable("Subjects", "Arts")
+                .checkResultTable("Hobbies", "Reading")
+                .checkResultTable("Picture", "structure.png")
+                .checkResultTable("Address", address)
+                .checkResultTable("State and City", "NCR Delhi");
     }
 }
