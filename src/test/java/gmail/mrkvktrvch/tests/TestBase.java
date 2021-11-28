812027771cf2bb1ecaa5f67584163dc906f5c479
so6er <mrkvktrvch@gmail.com>
2021-11-28 12:30:12 +0300

    added code for parameterized build in Jenkins

--- src/test/java/gmail/mrkvktrvch/tests/TestBase.java
+++ src/test/java/gmail/mrkvktrvch/tests/TestBase.java
@@ -2,16 +2,20 @@ package gmail.mrkvktrvch.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import gmail.mrkvktrvch.config.CredentialsConfig;
 import gmail.mrkvktrvch.helpers.Attach;
 import gmail.mrkvktrvch.pages.RegistrationPage;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import static com.codeborne.selenide.Selenide.open;
+import static java.lang.String.format;
 
 public class TestBase {
+
+    public static CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
     RegistrationPage registrationPage = new RegistrationPage();
 
@@ -19,7 +23,8 @@ public class TestBase {
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = format("https://%s:%s@%s", credentials.login(), credentials.password(),
+                System.getProperty("url"));
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
